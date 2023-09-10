0f7f4b063ddcfe64b030938a836dac362f236a56
Paul <paulbellet@hotmail.fr>
2023-09-10 22:10:20 +0200

    install redux

--- FRONTEND/my-react-app/src/store.js
+++ FRONTEND/my-react-app/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'; 
 import authReducer from './reducers/authreducers';
 import userReducer from './reducers/userreducer'; 
@@ -13,6 +13,7 @@ const rootReducer = combineReducers({
 // Middleware Redux Thunk pour gérer les actions asynchrones
 const middleware = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 export default store;
